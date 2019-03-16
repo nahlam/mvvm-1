2db9fbea630761eb306edd0b9a83d15846587a6c
Nahla Mortada <nahla.mortada@gmail.com>
2019-03-16 22:58:26 +0200

    feat: test the data

--- BSMVVM/App/Models/Article.swift
+++ BSMVVM/App/Models/Article.swift
@@ -12,7 +12,6 @@ struct Article: Decodable {
     
     let url: String
     let adxKeywords: String
-    let column: String?
     let section: String
     let byline: String
     let type: String
@@ -23,10 +22,6 @@ struct Article: Decodable {
     let id: Int
     let assetId: Int
     let views: Int
-    let desFacet: [String]?
-    let orgFacet: [String]?
-    let perFacet: [String]?
-    let geoFacet: String?
     let media: [Media]?
     let uri: String?
     
@@ -34,7 +29,6 @@ struct Article: Decodable {
         
         case url
         case adxKeywords = "adx_keywords"
-        case column
         case section
         case byline
         case type
@@ -45,10 +39,6 @@ struct Article: Decodable {
         case id
         case assetId = "asset_id"
         case views
-        case desFacet = "des_facet"
-        case orgFacet = "org_facet"
-        case perFacet = "per_facet"
-        case geoFacet = "geo_facet"
         case media
         case uri
     }
@@ -57,11 +47,11 @@ struct Article: Decodable {
 struct Media: Decodable {
     
     let type: String
-    let subtype: String
-    let caption: String
-    let copyright: String
-    let approvedForSyndication: Int
-    let mediaMetadata: [MediaMetadata]
+    let subtype: String?
+    let caption: String?
+    let copyright: String?
+    let approvedForSyndication: Int?
+    let mediaMetadata: [MediaMetadata]?
     
     enum CodingKeys: String, CodingKey {
         
--- BSMVVMTests/BSMVVMTests.swift
+++ BSMVVMTests/BSMVVMTests.swift
@@ -19,16 +19,26 @@ class BSMVVMTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testParsingData() {
+        guard let result = try? ArticlesResonse.fromJSON("Response", bundle: Bundle(for: type(of: self))) as ArticlesResonse else {
+            XCTFail("Failed to load the file")
+            return }
+        print(result.count)
+        XCTAssert(result.count == 1715, "Total count is wrong")
+        XCTAssert(result.articles.count == 20, "Articles count is wrong")
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+}
+
+extension Decodable {
+    
+    static func fromJSON<T: Decodable>(_ fileName: String, fileExtension: String = "json", bundle: Bundle = .main) throws -> T {
+        guard let url = bundle.url(forResource: fileName, withExtension: fileExtension) else {
+            throw NSError(domain: NSURLErrorDomain, code: NSURLErrorResourceUnavailable)
         }
+        
+        let data = try Data(contentsOf: url)
+        
+        return try JSONDecoder().decode(T.self, from: data)
     }
-
 }
